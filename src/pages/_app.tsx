6e048767c94f7152cb56b867e1d1932db26cf4b7
Alpesh Joshi <ajoshi@horizontalintegration.com>
2023-02-18 22:53:56 +0530

    Added tailwind config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,10 +8,10 @@ import { SitecorePageProps } from 'lib/page-props';
 //  bootstrap is used here to provide a clean layout for samples, without needing extra CSS in the sample app
 //  nprogress provides a loading indicator on page/route changes
 // Remove these in package.json as well if removed here.
-import 'bootstrap/dist/css/bootstrap.css';
-import 'nprogress/nprogress.css';
-import 'assets/app.css';
-import 'assets/Home.module.css';
+// import 'bootstrap/dist/css/bootstrap.css';
+// import 'nprogress/nprogress.css';
+// import 'assets/app.css';
+// import 'assets/Home.module.css';
 import 'assets/main.css';
 
 NProgress.configure({ showSpinner: false, trickleSpeed: 100 });
